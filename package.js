ae464b431cdddf1c652dea6a39f87a64b597a90c
zodern <zodern@users.noreply.github.com>
2022-07-12 15:00:45 -0500

    v1.0.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "zodern:types",
   summary: "Type definitions for Meteor packages",
   documentation: "./README.md",
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/zodern/meteor-types.git'
 });
 
