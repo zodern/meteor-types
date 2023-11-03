4f96432f34c630bd14f115c9cd1fa12c94fdd2e6
SimonVanneste <38662306+SimonVanneste@users.noreply.github.com>
2023-11-03 17:58:53 +0100

    Only register .d.ts compiler when publishing

--- publish-packages.js
+++ publish-packages.js
@@ -11,24 +11,27 @@ const PackageSource = require('./tools-imports.js').PackageSource;
 // 2) Generate types if needed
 // 3) Uninstall npm dev dependencies, and reinstall them after publishing
 
-Plugin.registerCompiler({
-  // These aren't used by the compiler
-  // This ensures meteor includes these files when building or publishing
-  // the package
-  extensions: ['d.ts', 'd.ts.map'],
-  filenames: ['package-types.json']
-}, () => new PackageCompiler());
+// Probably isn't completely reliable, but is close enough
+const isPublish = process.argv.includes('publish');
 
-class PackageCompiler {
-  processFilesForTarget(files) {
-    // Do nothing
-    // Meteor Typescript compilers usually ignore .d.ts files
-    // and package-types.json are not used during runtime
+if (isPublish) {
+  Plugin.registerCompiler({
+    // These aren't used by the compiler
+    // This ensures meteor includes these files when building or publishing
+    // the package
+    extensions: ['d.ts', 'd.ts.map'],
+    filenames: ['package-types.json']
+  }, () => new PackageCompiler());
+
+  class PackageCompiler {
+    processFilesForTarget(files) {
+      // Do nothing
+      // Meteor Typescript compilers usually ignore .d.ts files
+      // and package-types.json are not used during runtime
+    }
   }
 }
 
-// Probably isn't completely reliable, but is close enough
-const isPublish = process.argv.includes('publish');
 const packagePath = process.cwd();
 let prepared = false;
 
